8ba138d64779713cd2d36a43f731264c2846430f
emmpaz <mannypaz15@gmail.com>
2025-01-28 12:50:54 -0700

    tweaks

--- docker/wordpress-cli/install.sh
+++ docker/wordpress-cli/install.sh
@@ -4,10 +4,11 @@ wp core install \
     --title="${WORDPRESS_WEBSITE_TITLE}" \
     --admin_user="${WORDPRESS_ADMIN_USER}" \
     --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-    --admin_email="${WORDPRESS_ADMIN_EMAIL}"
+    --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+    --allow-root
 
-wp option update siteurl "${WORDPRESS_WEBSITE_URL}";
-wp option update users_can_register 1
+wp option update siteurl "${WORDPRESS_WEBSITE_URL}" --allow-root
+wp option update users_can_register 1 --allow-root
 
 cd wp-content/plugins
 
